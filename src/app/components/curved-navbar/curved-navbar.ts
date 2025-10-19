81d3cc761aacade01349773faa5744a050d8f40f
N <n47@github.com>
2025-10-19 13:11:23 +0530

    Added nav bar

---
+++ src/app/components/curved-navbar/curved-navbar.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'curved-navbar',
+  imports: [],
+  templateUrl: './curved-navbar.html',
+  styleUrl: './curved-navbar.scss'
+})
+export class CurvedNavbar {
+
+}
